50881036d9ea7500ee041b2b0d9adbc926614927
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-10-27 13:58:13 +0100

    Add styling

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
 
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
